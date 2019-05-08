e6b851b0c5b4ce2cfe0cfdd2b8ef8edc4a5c1b34
Derek <derek.baker89@gmail.com>
2019-05-08 03:52:13 +0100

    working spotify web player

---
+++ src/config_example.js
@@ -0,0 +1,10 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+//
+export const clientId = "9a8437237d05450fab2b1293bc76eb7d";
+export const redirectUri = "http://localhost:3000/redirect";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
